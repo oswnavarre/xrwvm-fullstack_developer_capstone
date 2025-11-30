6f2f298ad086450a7a598b07413e6f22a8ba4f9b
Oswaldo Navarrete <oswaldo.navarrete@gmail.com>
2025-11-30 01:46:56 -0600

    Refactor car make instance creation in populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -13,7 +13,6 @@ def initiate():
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
-
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0]},
@@ -35,4 +34,3 @@ def initiate():
 
     for data in car_model_data:
         CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
-    
