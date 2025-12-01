c1976c8da2a7ce07c51090b9b7805418d98d7d16
Oswaldo Navarrete <oswaldo.navarrete@gmail.com>
2025-11-30 22:05:36 -0600

    fixing settings2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 
-                 'https://onavarrete1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://onavarrete1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.
+    'proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://onavarrete1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://onavarrete1-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -97,7 +100,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
