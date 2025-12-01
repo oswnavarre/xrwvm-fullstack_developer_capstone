7a11161dd5025efc97907b78b4da32fec0437d8d
Oswaldo Navarrete <oswaldo.navarrete@gmail.com>
2025-11-30 21:43:12 -0600

    fixing views 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -150,8 +150,9 @@ def add_review(request):
         try:
             post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401,"message":
-                                 "Error in posting review"})
+        except Exception as e:
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"}
+            )
     else:
-        return JsonResponse({"status": 403,"message": "Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
